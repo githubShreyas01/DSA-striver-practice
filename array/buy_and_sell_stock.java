60ae808cfc585e4d1081ee29ef68aec01c1ed1ba
SHREYAS VERMA <94172366+githubShreyas01@users.noreply.github.com>
2024-07-28 12:24:25 +0530

    Create buy_and_sell_stock.java

---
+++ array/buy_and_sell_stock.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int mini = prices[0];
+        int prof = 0;
+        int cost =0;
+
+        for(int i =1; i<n;i++){
+            cost = prices[i] - mini;
+            prof = Math.max(prof, cost);
+            mini = Math.min(mini, prices[i]);
+        }
+
+        return prof;
+    }
+}
