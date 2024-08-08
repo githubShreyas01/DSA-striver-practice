0faa830d04da5a31a89f7d04f3ca06364f3f6ffa
SHREYAS VERMA <94172366+githubShreyas01@users.noreply.github.com>
2024-08-08 14:43:25 +0530

    Create sortLL0's1's2's.java

---
+++ linkedlist/sortLL0's1's2's/sortLL0's1's2's.java
@@ -0,0 +1,54 @@
+// } Driver Code Ends
+// User function Template for Java
+
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int data)
+    {
+        this.data = data;
+        next = null;
+    }
+}
+*/
+class Solution {
+    // Function to sort a linked list of 0s, 1s and 2s.
+    static Node segregate(Node head) {
+        // add your code here
+        if(head == null || head.next == null) return head;
+        
+        Node zeroHead= new Node(-1);
+        Node oneHead= new Node(-1);
+        Node twoHead= new Node(-1);
+        
+        Node zero=zeroHead;
+        Node one=oneHead;
+        Node two=twoHead;
+        
+        Node temp = head;
+        
+        while(temp!=null){
+            if(temp.data==0){
+                zero.next=temp;
+                zero=temp;
+            }
+            else if(temp.data==1){
+                one.next=temp;
+                one=temp;
+            }
+            else{
+                two.next=temp;
+                two=temp;
+            }
+            temp=temp.next;
+        }
+        
+        zero.next=(oneHead.next!=null)?oneHead.next:twoHead.next;
+        one.next=twoHead.next;
+        two.next=null;
+        
+        return zeroHead.next;
+    }
+}
