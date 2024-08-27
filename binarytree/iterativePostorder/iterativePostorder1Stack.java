70778e18b2d4ce75f18f48bc9cb08bee7fed7bd4
SHREYAS VERMA <94172366+githubShreyas01@users.noreply.github.com>
2024-08-27 18:17:27 +0530

    Create iterativePostorder1Stack.java

---
+++ binarytree/iterativePostorder/iterativePostorder1Stack.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode curr = root;
+
+        while(curr != null || !st.isEmpty()){
+            if(curr != null){
+                st.push(curr);
+                curr = curr.left;
+            }
+            else{
+                TreeNode temp = st.peek().right;
+                if(temp == null){
+                    temp = st.pop();
+                    ans.add(temp.val);
+                    while(!st.isEmpty() && temp == st.peek().right){
+                        temp = st.pop();
+                        ans.add(temp.val);
+                    }
+                }
+                else{
+                    curr = temp;
+                }
+            }
+        }
+        return ans;
+    }
+}
