1457c730efb320f42be4bb7aa456807807f19808
SHREYAS VERMA <94172366+githubShreyas01@users.noreply.github.com>
2024-09-11 15:12:55 +0530

    Create minInBST.java

---
+++ binarySearchTree/minInBST/minInBST.java
@@ -0,0 +1,24 @@
+// } Driver Code Ends
+/*
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+class Solution {
+    // Function to find the minimum element in the given BST.
+    int minValue(Node root) {
+        // code here
+        int min = root.data;
+        while(root.left != null){
+            root = root.left;
+        }
+        return root.data;
+    }
+}
